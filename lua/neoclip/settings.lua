04d86fa020de1ca0d813b408514884a6f98669db
Jean Sidharta <jeansidharta@gmail.com>
2023-01-16 19:25:28 -0300

    feat: closing telescope after action is now optional
    This commit adds a new setting at `on_select`, `on_paste` and `on_replay` called `close_telescope` (defaults to true), which when set to true, will close telescope after the action. If set to false, the action will be executed while maintaining the telescope window open.

--- lua/neoclip/settings.lua
+++ lua/neoclip/settings.lua
@@ -14,14 +14,20 @@ local settings = {
     content_spec_column = false,
     on_select = {
         move_to_front = false,
+        close_telescope = true,
     },
     on_paste = {
         set_reg = false,
         move_to_front = false,
+        close_telescope = true,
     },
     on_replay = {
         set_reg = false,
         move_to_front = false,
+        close_telescope = true,
+    },
+    on_custom_action = {
+        close_telescope = true,
     },
     keys = {
         telescope = {
